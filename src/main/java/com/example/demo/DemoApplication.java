071041d7a62c1aafbd4977cbe8ac7bc22bd6ca71
Anh Nguyen Hoang <nhanh36@gmail.com>
2023-11-10 23:15:35 +0700

    fixxx

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,20 +1,12 @@
 package com.example.demo;
 
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication extends SpringBootServletInitializer {
-
-
-
 
 	@Override
     protected SpringApplicationBuilder configure(
